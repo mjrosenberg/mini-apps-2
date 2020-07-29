d9fda434b9d99f43d3ae92dd9a909bcc3dd73938
mjrosenberg <mjr70@duke.edu>
2020-07-29 16:31:27 -0700

    implemented pagination and search

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -8,24 +8,57 @@ class App extends React.Component{
     super(props);
     this.state = {
       events: [],
-      page: 1,
+      page: 0,
       keyword: '',
       resultsText: 'All',
+      totalPageCount: 0,
+      pageCount: 20, //don't hard code this pull it down from the data
     };
     this.getEvents = this.getEvents.bind(this);
     this.changePage = this.changePage.bind(this);
     this.search = this.search.bind(this);
     this.changeKeyword = this.changeKeyword.bind(this);
+    this.getAll = this.getAll.bind(this);
+    this.resetSearch = this.resetSearch.bind(this);
   }
 
   componentDidMount(){
+    this.getAll('');
     this.getEvents();
+    this.setState({
+      totalPageCount: this.state.pageCount,
+    })
+  }
+
+  getAll(suffix){
+    fetch(`http://localhost:3000/events${suffix}`)
+    .then(response => {
+      return response.json()
+    })
+    .then((data) => {
+      if (suffix !== ''){
+        // console.log('data is', data);
+        this.setState({
+          pageCount: data.length/10,
+        });
+      } else {
+        this.setState({
+          pageCount: data.length/10,
+          totalPageCount: data.length/10,
+        });
+      }
+    })
+    .catch((err)=>{
+      console.log('err');
+    });
   }
 
   getEvents(){
     const url = `http://localhost:3000/events?_page=${this.state.page}&_limit=10`;
     fetch(url)
-    .then(response => response.json())
+    .then(response => {
+      return response.json()
+    })
     .then((data) => {
       this.setState({
         events: data,
@@ -39,11 +72,13 @@ class App extends React.Component{
 
   search(){
     // const keyword = document.getElementById('search').value;
-    const url = `http://localhost:3000/events?q=${this.state.keyword}&_page=${1}`;
+    this.getAll(`?q=${this.state.keyword}`);
+    const url = `http://localhost:3000/events?q=${this.state.keyword}&_page=1`;
     // const url = 'http://localhost:3000/events?q=Greece';
     fetch(url)
     .then(response => response.json())
     .then((data) => {
+      console.log('data from 2nd query is', data);
       this.setState({
         events: data,
         page: 1,
@@ -55,10 +90,10 @@ class App extends React.Component{
     });
   }
 
-  changePage(){
-    setState({
-      page: this.state.page+1,
-    })
+  changePage(e){
+    this.setState({
+      page: e.selected + 1,
+    }, this.getEvents);
   }
 
   changeKeyword(e){
@@ -67,14 +102,37 @@ class App extends React.Component{
       keyword: e.target.value,
     });
   }
+
+  resetSearch(){
+    this.getEvents();
+    this.setState({
+      pageCount: this.state.totalPageCount,
+      keyword: '',
+    });
+    document.getElementById('search').value = '';
+  }
+
   render(){
     return (
     <div id='main wrapper'>
       <label for="search">Keyword:</label>
-      {/* <input type="text" id="fname" name="fname" value="John"><br><br></br> */}
       <input type='text' id='search' name='search' onChange={this.changeKeyword}></input><br></br>
-      <button id='searchButton' onClick={this.search}>Search</button><br></br>
+      <button id='searchButton' onClick={this.search}>Search</button>
+      <button id='reset' onClick={this.resetSearch}>Reset Search</button><br></br>
       <div id='results'>{this.state.resultsText} Results</div>
+      <ReactPaginate
+          previousLabel={'previous'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={this.state.pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.changePage}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
       {this.state.events.map((event) => {
         // console.log('event is', event);
         return(
@@ -82,6 +140,7 @@ class App extends React.Component{
           <Event event={event}/>
         );
       })}
+
     </div>
     );
   }

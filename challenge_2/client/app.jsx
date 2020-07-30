b2ae4dc5538b27eeb471e90d05a0833ca2e9146b
mjrosenberg <mjr70@duke.edu>
2020-07-30 11:46:56 -0700

    ability to input custom dates

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -8,13 +8,14 @@ class App extends React.Component{
     super(props);
     this.state = {
       ticker: 'BTC',
-      startDate: '2017-01-01',
-      endDate: '2018-01-01',
+      startDate: '2019-01-01',
+      endDate: '2020-01-01',
       data: {},
       disclaimer: '',
       hasData: false,
     };
     this.getBTCData = this.getBTCData.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount(){
     this.getBTCData();
@@ -32,13 +33,33 @@ class App extends React.Component{
       })
       .catch((err)=>{
         console.log(err);
+        this.setState({
+          disclaimer: 'invalid date chosen'
+        })
       });
+  }
+
+  handleChange(e){
+    console.log(e.target.name, e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+
   }
 
   render(){
     if (this.state.hasData) {
       return(
         <div id='mainWrapper'>
+          <label>
+            Start Date:
+            <input type="date" name="startDate" value={this.state.startDate} onChange={this.handleChange} min="2011-01-01"></input>
+          </label>
+          <label>
+            End Date:
+            <input type="date" name="endDate" value={this.state.endDate} onChange={this.handleChange} min={this.state.startDate}></input>
+          </label>
+          <button id='regenerate' onClick={this.getBTCData}>Generate Price Graph</button>
           <PriceChart data={this.state.data} />
           <div id='disclaimer'>{this.state.disclaimer}</div>
         </div>

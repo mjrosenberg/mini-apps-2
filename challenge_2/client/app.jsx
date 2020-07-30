4aa2bd1c59d5ebb8cc15324cc4a8cf4ff9ada7a8
mjrosenberg <mjr70@duke.edu>
2020-07-30 09:46:37 -0700

    route to get prices written

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -5,16 +5,45 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-
+      ticker: 'BTC',
+      startDate: '2017-01-01',
+      endDate: '2018-01-01',
+      data: {},
+      disclaimer: '',
+      hasData: false,
     };
+    this.getBTCData = this.getBTCData.bind(this);
+  }
+  componentDidMount(){
+    this.getBTCData();
+  }
+  getBTCData(){
+    const url = `http://localhost:3000/price/between/${this.state.startDate}/${this.state.endDate}`;
+    fetch(url)
+      .then(response => response.json())
+      .then((data)=>{
+        this.setState({
+          data: data.bpi,
+          disclaimer: data.disclaimer,
+          hasData: true,
+        });
+      })
+      .catch((err)=>{
+        console.log(err);
+      });
   }
 
   render(){
-    return(
-      <div id='mainWrapper'>
-        Hello World
-      </div>
-    );
+    if (this.state.hasData) {
+      return(
+        <div id='mainWrapper'>
+          Hello World
+          {/* {this.state.data} */}
+        </div>
+      );
+    } else {
+      return(<div></div>);
+    }
   }
 }
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -9,6 +10,18 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
+
+app.get('/price/between/:startDate/:endDate', (req,res) => {
+  const url = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${req.params.startDate}&end=${req.params.endDate}`
+  fetch(url)
+    .then(response => response.json())
+    .then((data)=>{
+      res.send(data);
+    })
+    .catch((err)=>{
+      res.send(err);
+    });
+});
 
 app.listen(port, ()=>{
   console.log(`listening on port ${port}`);

9717ecc5610df5b50325c70fcfbb4158b4619b86
mjrosenberg <mjr70@duke.edu>
2020-07-30 09:22:11 -0700

    working server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
 
+const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.listen(port, ()=>{
+  console.log(`listening on port ${port}`);
+});

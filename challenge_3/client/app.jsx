1622bbc061b0d6191a56559fe95be4f76ede5af1
mjrosenberg <mjr70@duke.edu>
2020-07-31 16:06:22 -0700

    implemented last turn logic

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -33,7 +33,7 @@ submitScore(e) {
     document.getElementById('throw').value = 0;
     console.log('throw score is', throwScore);
 
-    if (this.state.frameNum < 10 && this.state.currThrow < 3){
+    if (this.state.frameNum < 10 || this.state.currThrow < 2){
       if (this.state.currThrow === 1){
         if (throwScore < 10) {
           document.getElementById(`firstThrow${this.state.frameNum}`).innerHTML = throwScore;
@@ -174,7 +174,63 @@ submitScore(e) {
       }
     } else {
       //implement logic for having a third throw if a spare or strike on the tenth
+      if (this.state.currThrow === 2){
+        if (!this.state.strike){
+          console.log('no strike before');
+          if (throwScore < 10 - this.state.frameScore){
+            //did not throw a spare
+            document.getElementById(`secondThrow${this.state.frameNum}`).innerHTML = throwScore;
+            let totFrameScore = parseInt(throwScore) + parseInt(this.state.frameScore);
+            this.setState({
+              currThrow: 3,
+              totScore: this.state.totScore + totFrameScore,
+              spare: false,
+            });
+          } else{
+            document.getElementById(`secondThrow${this.state.frameNum}`).innerHTML = '\\';
+            this.setState({
+              currThrow: 3,
+              totScore: this.state.totScore + 10,
+              spare: true,
+            });
+          }
+        } else {
+          //if a strike was thrown on the previous turn
+          console.log('strike before');
 
+          if (throwScore < 10 - this.state.frameScore){
+            //did not throw a spare
+            document.getElementById(`secondThrow${this.state.frameNum}`).innerHTML = throwScore;
+            let prevFrameScore = parseInt(this.state.frameScore) + parseInt(throwScore);
+            this.setState({
+              currThrow: 3,
+              totScore: parseInt(this.state.totScore) + parseInt(prevFrameScore) + parseInt(prevFrameScore),
+              strike: false,
+              spare: false,
+              //update the total score to add 10 after the second frame and then just add the extra on this frame and append it to the last frame's score
+            });
+          } else{
+            document.getElementById(`secondThrow${this.state.frameNum}`).innerHTML = '\\';
+            let prevFrameScore = parseInt(this.state.frameScore) + parseInt(throwScore) + 10;
+            this.setState({
+              currThrow: 3,
+              totScore: parseInt(this.state.totScore) + parseInt(prevFrameScore) + parseInt(prevFrameScore),
+              strike: false,
+              spare: true,
+              //update the total score to add 10 after the second frame and then just add the extra on this frame and append it to the last frame's score
+            });
+          }
+        }
+      } else {
+        this.setState({
+          frameNum: 11,
+          totScore: this.state.totScore + parseInt(throwScore),
+          currThrow: 1,
+        }, ()=>{
+          document.getElementById(`totScore${this.state.frameNum - 1}`).innerHTML = this.state.totScore;
+        })
+      }
+      }
     }
   }
 
